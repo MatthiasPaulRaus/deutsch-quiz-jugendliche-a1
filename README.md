c5447083c85cee22e2dc471d1fcde18c5c9fd065
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-03-23 11:48:59 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+deutsch-quiz-jugendliche-a1
+Ein Quiz für jugendliche Deutschlernende auf dem Niveau A1. Inhalt der 30 Aufgaben sind Grammatik, Strukturen und Wortschatz.
---
+++ aufgaben.js
@@ -0,0 +1,183 @@
+const aufgaben = [
+{
+	"question": "Hallo! Ich heiße Matthias. Und wie heißt du?",
+	"option1": "Sie heißt Clara.",
+	"option2": "Ich heiße Clara.",
+	"option3": "Du heißt Clara.",
+	"answer": "2"
+}, {
+	"question": "Guten Tag! Ich bin Frau Müller. Und wie heißt du?",
+	"option1": "Ich bin Clara.",
+	"option2": "Ich bin Blanco.",
+	"option3": "Ich heiße Clara Blanco.",
+	"answer": "3"
+}, {
+	"question": "Ich komme aus Deutschland. Woher kommst du?",
+	"option1": "Aha, ich komme aus Italien.",
+	"option2": "Du kommst aus Spanien.",
+	"option3": "Wir kommen aus Spanien.",
+	"answer": "1"
+}, {
+	"question": "Ich wohne in München. Wo wohnst du?",
+	"option1": "Sie wohnen in München.",
+	"option2": "Du wohnst in München.",
+	"option3": "Ich wohne auch in München.",
+	"answer": "3"
+},  {
+	"question": "Ich wohne in Hamburg. Und wo wohnt ihr?",
+	"option1": "Wir wohnen in München.",
+	"option2": "Sie wohnen in Berlin.",
+	"option3": "Ihr wohnt in Hamburg.",
+	"answer": "1"
+}, {
+	"question": "Clara, wer ist das?",
+	"option1": "Das ist ein Auto.",
+	"option2": "Hm, das ist eine Schule.",
+	"option3": "Das ist meine Schwester Olga.",
+	"answer": "3"
+}, {
+	"question": "Wie viel kostet das Fahrrad?",
+	"option1": "Er kostet 59 Euro.",
+	"option2": "Der kostet 59 Euro.",
+	"option3": "Es kostet 159 Euro.",
+	"answer": "3"
+}, {
+	"question": "Wie heißt das auf Deutsch?",
+	"option1": "Sie ist eine Uhr.",
+	"option2": "Das ist eine Uhr.",
+	"option3": "Das ist keine Uhr.",
+	"answer": "2"
+}, {
+	"question": "Wie viele Handys hast du?",
+	"option1": "Er hat drei Handys.",
+	"option2": "Ich habe zwei Handys.",
+	"option3": "Ich habe moderne Handys.",
+	"answer": "2"
+}, {
+	"question": "Wie findest du das T-Shirt?",
+	"option1": "Ich finde sie nicht schön.",
+	"option2": "Wow, das gefällt mir sehr gut.",
+	"option3": "Ich finde sie nicht praktisch.",
+	"answer": "2"
+}, {
+	"question": "Kannst du schwimmen?",
+	"option1": "Nein, du kannst nicht schwimmen.",
+	"option2": "Wir können nicht schwimmen.",
+	"option3": "Ja, ich kann sehr gut schwimmen.",
+	"answer": "3"
+}, {
+	"question": "Was machst du in deiner Freizeit?",
+	"option1": "Sie hat keine Freizeit.",
+	"option2": "Ich gehe gern Joggen.",
+	"option3": "Ich kann nicht Gitarre spielen.",
+	"answer": "2"
+}, {
+	"question": "Wann ist das Wochenende?",
+	"option1": "Am Samstag und Sonntag.",
+	"option2": "Am Montag und Dienstag.",
+	"option3": "Am Donnerstag und Freitag.",
+	"answer": "1"
+}, {
+	"question": "Wann stehst du auf?",
+	"option1": "Wir stehen um acht auf.",
+	"option2": "Sie muss um acht aufstehen.",
+	"option3": "Ich stehe um acht auf.",
+	"answer": "3"
+}, {
+	"question": "Wie spät ist es?",
+	"option1": "Es ist fünfzehn vor halb sechs.",
+	"option2": "Es ist fünfzehn.",
+	"option3": "Es ist Viertel vor fünf.",
+	"answer": "3"
+},  {
+	"question": "Wann frühstückst du?",
+	"option1": "Hm, ich frühstücke von acht bis halb neun.",
+	"option2": "Von 12 bis 13 Uhr.",
+	"option3": "Ich trinke einen Tee.",
+	"answer": "1"
+}, {
+	"question": "Gibt es einen Bus zur Schule?",
+	"option1": "Ja, es gibt eine Schule.",
+	"option2": "Es gibt keine Schule.",
+	"option3": "Ja, es gibt einen Bus vom Bahnhof zur Schule.",
+	"answer": "3"
+}, {
+	"question": "Kannst du mich morgen anrufen?",
+	"option1": "Ich telefoniere am Nachmittag.",
+	"option2": "Ja, ich rufe dich am Nachmittag an.",
+	"option3": "Ich habe dein Handy nicht.",
+	"answer": "2"
+}, {
+	"question": "Was hast du gestern Abend gemacht?",
+	"option1": "Wir haben eingekauft.",
+	"option2": "Ich habe mein Zimmer aufgeräumt.",
+	"option3": "Ich muss aufräumen.",
+	"answer": "2"
+}, {
+	"question": "Was hast du am Sonntag gemacht?",
+	"option1": "Ich habe am Freitag Mathe gelernt.",
+	"option2": "Am Sonntag möchte ich Sport machen.",
+	"option3": "Ich habe eine Freundin besucht.",
+	"answer": "3"
+}, {
+	"question": "Wie hast du deinen Geburtstag gefeiert?",
+	"option1": "Ich will eine Party machen.",
+	"option2": "Ich habe meine Freunde eingeladen.",
+	"option3": "Na, ich habe viele Sachen geschenkt.",
+	"answer": "2"
+}, {
+	"question": "Entschuldigung, wo ist denn hier der Bahnhof?",
+	"option1": "Geh weiter!",
+	"option2": "Sie sehen ihn nicht.",
+	"option3": "Geh hier rechts und dann geradeaus.",
+	"answer": "3"
+},  {
+	"question": "Wann können wir uns treffen?",
+	"option1": "Um halb acht können wir uns treffen.",
+	"option2": "Sie treffen sich am Montag vor der Schule.",
+	"option3": "Sie haben keine Zeit.",
+	"answer": "1"
+}, {
+	"question": "Wo wart ihr in den Ferien?",
+	"option1": "Sie hatten keine Ferien.",
+	"option2": "Wir waren zwei Wochen in Italien.",
+	"option3": "Wir haben keine Reise gemacht.",
+	"answer": "2"
+}, {
+	"question": "He, wie findest du meine Hose?",
+	"option1": "Die gefällt mir sehr gut.",
+	"option2": "Sie gefallen uns gut.",
+	"option3": "Das gefällt mir nicht.",
+	"answer": "1"
+}, {
+	"question": "Ich trage gern T-Shirts. Und du?",
+	"option1": "Sie tragen auch gern T-Shirts.",
+	"option2": "Wir tragen Hemden.",
+	"option3": "Ich trage am liebsten Pullover.",
+	"answer": "3"
+},  {
+	"question": "Wann ist dein Geburtstag?",
+	"option1": "Am vierzehnten August.",
+	"option2": "Ich werde vierzehn.",
+	"option3": "Vierzehn Jahre alt.",
+	"answer": "1"
+}, {
+	"question": "Was hat dir am besten gefallen?",
+	"option1": "Das Zentrum hat uns sehr gefallen.",
+	"option2": "Uns gefällt der große Sportplatz.",
+	"option3": "Am besten fand ich den Sportplatz.",
+	"answer": "3"
+}, {
+	"question": "Warum bist du gestern nicht gekommen?",
+	"option1": "Ich hatte keine Lust.",
+	"option2": "Ich wollte kommen.",
+	"option3": "Wir waren gestern im Kino.",	
+	"answer": "1"
+}, {
+	"question": "Können wir den Englisch-Test verschieben?",
+	"option1": "Nein, wir haben keinen Test.",
+	"option2": "Ja, wir können ihn nächste Woche machen.",
+	"option3": "Nein, sie verschieben nichts.",
+	"answer": "2"
+} 
+];
